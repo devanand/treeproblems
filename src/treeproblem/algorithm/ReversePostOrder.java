6cc6c4e385dc62d248730df5e17cd24f95feb65e
devanand <anand.dev.1985@gmail.com>
2016-02-06 16:15:22 +0530

    Added changes

--- src/treeproblem/algorithm/ReversePostOrder.java
+++ src/treeproblem/algorithm/ReversePostOrder.java
@@ -1,6 +1,10 @@
 package treeproblem.algorithm;
 
+import java.lang.reflect.Array;
+import java.util.Collections;
+
 import treeproblem.node.Node;
+import treeproblem.util.Util;
 
 public class ReversePostOrder {
 
@@ -16,5 +20,12 @@ public class ReversePostOrder {
 		
 		arrayToPostOrder(a, root.left, start, pivot-1);
 		arrayToPostOrder(a, root.right, pivot+1, end-1);
+	}
+	
+	public static void main(String[] args) {
+		int a[] = {8, 9, 4, 10, 11, 5, 2, 12, 13, 6, 14, 15, 7, 3, 1};
+		Node root = new Node(a[a.length-1]);
+		arrayToPostOrder(a, root, 0, a.length-2);
+		Util.printTree(Collections.singletonList(root), 1, 4);
 	}
 }
