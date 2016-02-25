bc658e4cfb2423a25bc9c7e1dd01faeed8973684
devanand <anand.dev.1985@gmail.com>
2016-02-25 09:48:33 +0530

    Added changes

---
+++ src/treeproblem/algorithm/TreeSum.java
@@ -0,0 +1,29 @@
+package treeproblem.algorithm;
+
+import java.util.Collections;
+
+import treeproblem.node.Node;
+import treeproblem.util.Util;
+
+public class TreeSum {
+	
+	public static int treeSum(Node node) {
+		if(node == null) {
+			return 0;
+		}
+		
+		int old = node.data;
+		
+		node.data = treeSum(node.left)+treeSum(node.right);
+		
+		return node.data+old;
+	}
+	
+	public static void main(String[] args) {
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		treeSum(root);
+		Util.printTree(Collections.singletonList(root), 1, 2);
+	}
+}
